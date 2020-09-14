c0bd2b60f558cd6dbd59031dfbd9ca5c722341b0
Mounika <mounikabalusani1@gmail.com>
2020-09-14 12:15:02 +0530

    second commit

--- src/test/java/qaacademy/MavenNew/SeleniumTest.java
+++ src/test/java/qaacademy/MavenNew/SeleniumTest.java
@@ -9,10 +9,12 @@ public class SeleniumTest {
 	public void seleniumlogin() {
 		
 		System.out.println("I am selenium login");
+		System.out.println("I am selenium login for gitDemo");
 	}
 	@Test
 	public void seleniumlogout() {
 		
 		System.out.println("I am selenium logout");
+		System.out.println("I am selenium logout for gitDemo");
 	}
 }
